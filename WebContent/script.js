b460a6f96df33fd226d9a72e7dd20e262f3c637b
shaleenanuj <anuj6491@gmail.com>
2016-06-20 21:18:15 +0530

    Commit done for the first time

---
+++ WebContent/script.js
@@ -0,0 +1,12 @@
+document.getElementById("foot01").innerHTML =
+"<p>* required fields</p>";
+
+document.getElementById("foot02").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " W3Schools. All rights reserved.</p>";
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href=index.html>TAN</a></li>" +
+"<li><a href=customers.html>PAN</a></li>" +
+"<li><a href=About.html>PAN with DOB</a></li>" +
+"</ul>";
